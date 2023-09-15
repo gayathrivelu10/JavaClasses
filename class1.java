33f71caf649bf0d92d0cf14b732659b528f1f885
gayathrivelu10 <gayathrisan47@gmail.com>
2023-09-15 19:40:37 +0530

    1st commit

---
+++ class1.java
@@ -0,0 +1,15 @@
+public class Math{
+    public void add(int a,int b){
+        int add=a+b;
+        System.out.println("Sum:"+add);
+    }
+    public void sub(int a,int b){
+        int sub=a+b;
+        System.out.println("sub:"+sub);
+    }
+    public static void main(String args[]){
+        Math m=new Math();
+        m.add(10, 10);
+        m.sub(100, 20);
+    }
+}
